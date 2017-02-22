f3283e56def06baed8901f9e0373bdc28951950c
Salimghb <sghbabra@hotmail.fr>
2017-02-22 20:29:27 +0100

    dsfdtfhhu

--- Symfony/src/Salim/PlateformeBundle/Controller/AdvertController.php
+++ Symfony/src/Salim/PlateformeBundle/Controller/AdvertController.php
@@ -10,10 +10,42 @@ class AdvertController extends Controller
 
 	public function indexAction()
 	{
-		$content = $this -> get('templating') -> render('SalimPlateformeBundle:Advert:index.html.twig', array('nom'=>'Ghbabra',
-			'prenom'=>'Salim'));
+		$content = $this -> get('templating') -> render(
+
+			'SalimPlateformeBundle:Advert:index.html.twig',
+			array('nom'=>'Ghbabra',
+				'prenom'=>'Salim'));
 
 		return new Response($content);
+
+	}
+
+
+	public function viewAction($id)
+	{
+
+		return new Response ("Affichage de l'annonce de l'id : ".$id);
+
+	}
+
+	public function viewSlugAction($slug,$year,$format)
+	{
+
+		return new Response (
+			"Affichage de l'annonce correspondante au slug '".$slug.
+			"', créée en ".$year." au format ".$format.".");
+
+	}
+
+	public function indexByeAction()
+	{
+		$contentBye = $this->get('templating') -> render (
+
+			'SalimPlateformeBundle:Advert:indexBye.html.twig', 
+			array('nom'=>'Ghbabra',
+				'prenom'=>'Salim'));
+
+		return new Response($contentBye);
 
 	}
 
